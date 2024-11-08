5bf4ee0f2d7491d4d54d01dd68135ef1a5d53710
kadmytro <karapostoldmytro@gmail.com>
2024-11-08 19:03:39 +0200

    implement image swipe

--- frontend/src/components/FullScreenImageViewer.tsx
+++ frontend/src/components/FullScreenImageViewer.tsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, CSSProperties } from "react";
+import React, { useState, useEffect, CSSProperties, useRef } from "react";
 import { PhotoCardProps } from "./PhotoCard";
-import { useSpring } from "react-spring";
-import { useDrag } from "@use-gesture/react";
 
 interface FullscreenImageViewerProps {
   items: PhotoCardProps[];
   selectedItemId: number;
   onClose: () => void;
 }
+
+type ScreenType = "wide" | "narrow" | "mobile";
 
 const FullscreenImageViewer: React.FC<FullscreenImageViewerProps> = ({
   items,
@@ -18,46 +18,113 @@ const FullscreenImageViewer: React.FC<FullscreenImageViewerProps> = ({
     items.findIndex((p) => p.id === selectedItemId)
   );
   const [fade, setFade] = useState(false);
-  const prevIndex = (currentIndex - 1 + items.length) % items.length;
-  const nextIndex = (currentIndex + 1) % items.length;
+  const [screenType, setScreenType] = useState<ScreenType>("wide");
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [offsetX, setOffsetX] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
-  const [{ x }, set] = useSpring(() => ({ x: 0 }));
+  const carouselRef = useRef<HTMLDivElement>(null);
 
-  const bind = useDrag(
-    ({ offset: [ox], velocity, direction: [dx], cancel }) => {
-      if (Math.abs(ox) > window.innerWidth / 2 || velocity[0] > 0.5) {
-        if (dx > 0) {
-          goToPrevious();
-        } else {
-          goToNext();
-        }
-        cancel();
-      } else {
-        set({ x: ox, immediate: true });
-      }
-    },
-    { axis: 'x' }
-  );
+  const onResize = () => {
+    const width = window.innerWidth;
+    let newScreenType: ScreenType;
 
-  const goToPrevious = () => {
-    setCurrentIndex((currentIndex - 1 + items.length) % items.length);
-    set({ x: 0, immediate: false });
+    if (width > 1000) {
+      newScreenType = "wide";
+    } else if (width > 750) {
+      newScreenType = "narrow";
+    } else {
+      newScreenType = "mobile";
+    }
+    setScreenType(newScreenType);
   };
-
-  const goToNext = () => {
-    setCurrentIndex((currentIndex + 1) % items.length);
-    set({ x: 0, immediate: false });
-  };
-
-  const currentPhoto = items[currentIndex];
-  const prevPhoto = items[prevIndex];
-  const nextPhoto = items[nextIndex];
 
   useEffect(() => {
-    setCurrentIndex(items.findIndex((p) => p.id === selectedItemId));
-  }, [selectedItemId]);
+    onResize();
+  });
 
-  const handleNext = () => {
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    if (isTransitioning) return;
+    setStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    setIsDragging(true);
+    const deltaX = e.touches[0].clientX - startX;
+    setOffsetX(deltaX);
+  };
+
+  const handleTouchEnd = () => {
+    if (!isDragging) return;
+    setIsTransitioning(true);
+    setIsDragging(false);
+    const containerWidth = window.innerWidth;
+    const relativeOffsetX = offsetX / containerWidth;
+    const newOffset =
+      relativeOffsetX < -0.2
+        ? -containerWidth
+        : relativeOffsetX > 0.2
+        ? containerWidth
+        : 0;
+    setOffsetX(newOffset);
+    const newIndex =
+      relativeOffsetX < -0.2
+        ? (currentIndex + 1) % items.length
+        : relativeOffsetX > 0.2
+        ? (currentIndex - 1 + items.length) % items.length
+        : currentIndex;
+    setTimeout(() => {
+      setCurrentIndex(newIndex);
+      setOffsetX(0);
+      setIsTransitioning(false);
+    }, 200);
+  };
+
+  const getTranslateValue = () => {
+    const translatePercentage = -100 + (offsetX / window.innerWidth) * 100;
+    return translatePercentage;
+  };
+
+  const getVisibleImages = () => {
+    const prevIndex = (currentIndex - 1 + items.length) % items.length;
+    const nextIndex = (currentIndex + 1) % items.length;
+    return [items[prevIndex], items[currentIndex], items[nextIndex]];
+  };
+
+  const visibleImages = getVisibleImages();
+
+  const calculateImageStyles = (image: PhotoCardProps): CSSProperties => {
+    const { innerWidth: windowWidth, innerHeight: windowHeight } = window;
+    const imageHeight = image.height;
+    const imageWidth = image.width;
+    const windowRatio =
+      screenType === "wide"
+        ? (windowWidth * 0.9 - 40) / (windowHeight * 0.95)
+        : windowWidth / windowHeight;
+    const imageRatio = imageWidth / imageHeight;
+
+    if (windowRatio > imageRatio) {
+      return {
+        height: "100%",
+        width: "auto",
+      };
+    } else {
+      return {
+        width: "100%",
+        height: "auto",
+      };
+    }
+  };
+  const handleNext = (e?: React.MouseEvent<HTMLButtonElement>) => {
+    e?.stopPropagation();
     setFade(true);
     setTimeout(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
@@ -65,7 +132,8 @@ const FullscreenImageViewer: React.FC<FullscreenImageViewerProps> = ({
     }, 150);
   };
 
-  const handlePrev = () => {
+  const handlePrev = (e?: React.MouseEvent<HTMLButtonElement>) => {
+    e?.stopPropagation();
     setFade(true);
     setTimeout(() => {
       setCurrentIndex(
@@ -93,56 +161,60 @@ const FullscreenImageViewer: React.FC<FullscreenImageViewerProps> = ({
     };
   }, []);
 
-  const calculateImageStyles = (): CSSProperties => {
-    const { innerWidth: windowWidth, innerHeight: windowHeight } = window;
-    const imageHeight = items[currentIndex].height;
-    const imageWidth = items[currentIndex].width;
-    const windowRatio = (windowWidth * 0.9 - 40) / (windowHeight * 0.95); //this includes the margins and other, in order to make precise calculations
-    const imageRatio = imageWidth / imageHeight;
-
-    if (windowRatio > imageRatio) {
-      // Window is wider than image, limit by height
-      return {
-        height: "100%",
-        width: "auto",
-      };
-    } else {
-      // Window is taller than image, limit by width
-      return {
-        width: "100%",
-        height: "auto",
-      };
-    }
-  };
-
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen backdrop-blur-lg bg-primary bg-opacity-70 flex justify-center items-center z-50">
-      <div className="relative flex items-center justify-center h-full w-full">
+    <div
+      className="fixed top-0 left-0 w-screen h-screen backdrop-blur-lg bg-primary bg-opacity-70 flex justify-center items-center z-50"
+      ref={carouselRef}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
+      <div className="relative flex items-center justify-center h-full w-full z-20">
         <button
-          className="w-5% h-80% absolute left-0 text-center align-middle bg-transparent border-none text-primaryText text-opacity-0 narrow:text-opacity-60 hover:text-opacity-100 hover:text-5xl transition-all text-4xl cursor-pointer select-none"
+          className="w-1/3 wide:w-1/5 h-full z-20 absolute left-0 text-left pl-3 bg-transparent border-none text-primaryText text-opacity-0 
+            narrow:text-opacity-60 narrow:hover:text-opacity-100 narrow:hover:text-5xl transition-all text-4xl cursor-pointer select-none"
           onClick={handlePrev}
         >
           ‹
         </button>
-        <div className="w-full narrow:w-90% h-full flex justify-center align-middle items-center my-0 narrow:mx-5">
-          <img
-            src={items[currentIndex].image}
-            alt={items[currentIndex].caption}
-            style={calculateImageStyles()}
-            className={`transition-opacity duration-200 ${
-              fade ? "opacity-40" : "opacity-100"
-            }`}
-          />
+        <div
+          className={`flex h-full items-center ${
+            offsetX !== 0 && !isDragging
+              ? " transition-transform ease-in-out"
+              : ""
+          }`}
+          style={{
+            transform: `translateX(${getTranslateValue()}%)`,
+          }}
+        >
+          {visibleImages.map((image, idx) => (
+            <div
+              key={image.id}
+              className="flex-shrink-0 content-center h-full w-full"
+              style={{ touchAction: "none" }}
+            >
+              <img
+                src={image.image}
+                alt={image.caption}
+                style={calculateImageStyles(image)}
+                draggable="false"
+                className={`mx-auto transition-opacity duration-200 ${
+                  fade ? "opacity-40" : "opacity-100"
+                }`}
+              />
+            </div>
+          ))}
         </div>
         <button
-          className="w-5% h-80% absolute right-0 text-center align-middle bg-transparent border-none text-primaryText text-opacity-0 narrow:text-opacity-60 hover:text-opacity-100 hover:text-5xl transition-all text-4xl cursor-pointer select-none"
+          className="w-1/3 wide:w-1/5 h-full z-20 absolute right-0 text-right pe-3 bg-transparent border-none text-primaryText text-opacity-0 
+            narrow:text-opacity-60 narrow:hover:text-opacity-100 narrow:hover:text-5xl transition-all text-4xl cursor-pointer select-none"
           onClick={handleNext}
         >
           ›
         </button>
       </div>
       <button
-        className="w-16 h-16 absolute text-center top-1 right-2 bg-transparent border-none text-primaryText text-opacity-60 hover:text-opacity-100 hover:text-5xl transition-all text-4xl cursor-pointer select-none"
+        className="w-9 h-9 z-50 narrow:w-16 narrow:h-16 absolute text-center top-1 right-1 bg-transparent border-none text-primaryText text-opacity-60 hover:text-opacity-100 hover:text-5xl transition-all text-4xl cursor-pointer select-none"
         onClick={onClose}
       >
         ×
